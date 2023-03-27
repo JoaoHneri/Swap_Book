5e0afb5cd5d7dff0f490623514135f9d6b6af6ae
Silas Matos <silas.progamacao@gmail.com>
2023-03-26 23:39:40 -0300

    footer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import CarouselFadeExample from './components/Carousel'
 import Categorias from './components/Categorias.js'
 import Navbar from './components/NavBar/Navbar'
 
+
+import Footer from './components/Footer'
+
 function App() {
   return (
     <div className=" ">
       <Navbar />
       <CarouselFadeExample />
       <Categorias />
-    </div>
+      <Footer />
+     
+         </div>
   )
 }
 
