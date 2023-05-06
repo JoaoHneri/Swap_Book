6c86ae75fac0ae62748fafb1cc92fd33d007672b
João Henrique <joaohnsousa@gmail.com>
2023-05-05 23:09:30 -0300

    Tentativa Deploy

--- Swap_Book-Front/src/Services/Api.js
+++ Swap_Book-Front/src/Services/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3333/"
+    baseURL: "https://swap-book-api.onrender.com"
 })
 
 export default api
--- Swap_Book-Front/src/index.js
+++ Swap_Book-Front/src/index.js
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'mapbox-gl/dist/mapbox-gl.css';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+if(process.env.NODE_ENV === 'production') disableReactDevTools()
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
